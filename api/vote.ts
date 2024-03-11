d00d1ec79adac342d018d56cfb14ef2dfecf3f0a
atxmqq <atomgamer123@gmail.com>
2024-03-11 22:18:15 +0700

    ท

--- api/vote.ts
+++ api/vote.ts
@@ -4,7 +4,7 @@ import { conn } from "../dbconn";
 export const router = express.Router();
 
 router.get("/", (req, res) => {
-    const sql = "select pictureAnime.*, vote.*, COALESCE(vote.score, 0) AS score FROM pictureAnime LEFT JOIN vote ON pictureAnime.pid = vote.pid_fk";
+    const sql = "select pictureAnime.*, vote.*, COALESCE(vote.score, 100) AS score FROM pictureAnime LEFT JOIN vote ON pictureAnime.pid = vote.pid_fk";
     conn.query(sql, (err, result) => {
         if (err) {
             res.json(err);

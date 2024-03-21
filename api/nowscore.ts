28e0b3ad1c7f2970f6b6364ade6bf1ab2e0fc56f
atxmqq <atomgamer123@gmail.com>
2024-03-21 22:19:55 +0700

    a

--- api/nowscore.ts
+++ api/nowscore.ts
@@ -3,13 +3,18 @@ import { conn } from "../dbconn";
 
 export const router = express.Router();
 
+
+//ดึงผลรวมคะแนนทั้งหมดที่ ไม่รวมปัจจุบัน
 router.get("/", (req, res) => {
     const sql = `
-        SELECT pictureAnime.*, COALESCE(SUM(CASE WHEN vote.vote_date != CURRENT_DATE THEN vote.score ELSE 0 END), 100) AS total_score 
-        FROM pictureAnime 
-        LEFT JOIN vote ON pictureAnime.pid = vote.pid_fk 
-        GROUP BY pictureAnime.pid;
+            SELECT pictureAnime.*, COALESCE(SUM(CASE WHEN vote.vote_date != CURRENT_DATE THEN vote.score ELSE 0 END), 0) AS total_score 
+            FROM pictureAnime 
+            LEFT JOIN vote ON pictureAnime.pid = vote.pid_fk 
+            GROUP BY pictureAnime.pid;
     `;
+    //ใช้ฟังก์ชัน COALESCE เพื่อรับผลลัพธ์ของ SUM ของคะแนนโหวต 
+    //โดยใช้ CASE WHEN ในการตรวจสอบว่า vote_date ไม่เท่ากับ CURRENT_DATE (วันปัจจุบัน) 
+    //หากเป็นเช่นนั้นจะให้คะแนนที่โหวตได้ ถ้าไม่ใช่จะให้คะแนนเป็น 0
     conn.query(sql, (err, result) => {
         if (err) {
             res.json(err);
@@ -37,6 +42,9 @@ router.get("/sevenday_before/:pid", (req, res) => {
         GROUP BY voting_day, pictureAnime.pid
         ORDER BY voting_day ASC;
     `;
+    //ใช้  CURDATE เพื่อให้ได้วันปัจจุบัน และ ใช้ DATE_SUB เพื่อหัก 1 วัน
+    //สร้างตารางชื่อ seq ที่ประกอบด้วยคอลัมน์เดียวชื่อ seq ซึ่งเป็นลำดับตัวเลขจาก 0 ถึง 7 (ช่วง 7 วัน)
+
     conn.query(sql, [pid], (err, result) => {
         if (err) {
             res.json(err);
--- api/vote.ts
+++ api/vote.ts
@@ -4,7 +4,8 @@ import { conn } from "../dbconn";
 export const router = express.Router();
 
 router.get("/", (req, res) => {
-    const sql = "select pictureAnime.*, vote.*, COALESCE(vote.score, 100) AS score FROM pictureAnime LEFT JOIN vote ON pictureAnime.pid = vote.pid_fk";
+    const sql =
+        "SELECT pictureAnime.*, SUM(COALESCE(vote.score, 0)) AS total_score FROM pictureAnime LEFT JOIN vote ON pictureAnime.pid = vote.pid_fk GROUP BY pictureAnime.pid";
     conn.query(sql, (err, result) => {
         if (err) {
             res.json(err);
@@ -13,6 +14,4 @@ router.get("/", (req, res) => {
         }
     });
 });
-
-
 

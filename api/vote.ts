03c2a03df248af775578280a3b7b2f48bce16262
atxmqq <atomgamer123@gmail.com>
2024-03-22 23:10:22 +0700

    s

--- api/vote.ts
+++ api/vote.ts
@@ -5,7 +5,7 @@ export const router = express.Router();
 
 router.get("/", (req, res) => {
     const sql =
-        "SELECT pictureAnime.*, SUM(COALESCE(vote.score, 100)) AS total_score FROM pictureAnime LEFT JOIN vote ON pictureAnime.pid = vote.pid_fk GROUP BY pictureAnime.pid";
+        "SELECT pictureAnime.*, SUM(COALESCE(vote.score, 0)) AS total_score FROM pictureAnime LEFT JOIN vote ON pictureAnime.pid = vote.pid_fk GROUP BY pictureAnime.pid";
     conn.query(sql, (err, result) => {
         if (err) {
             res.json(err);
